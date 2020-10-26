013f1de73096e50f0779455df328b1e408593050
brynknowles <bryn.knowles@gmail.com>
2020-10-26 16:57:31 -0400

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+// no longer has DOM node 'main#main'
+main.remove()
+
+// has a 'newHeader' variable that points to node 'h1#victory'
+const newHeader = document.createElement('h1')
+
+// has a 'newHeader' variable that points to node 'h1#victory'
+newHeader.id = 'victory'
+
+// has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.innerHTML = "Bryn is the champion!"
